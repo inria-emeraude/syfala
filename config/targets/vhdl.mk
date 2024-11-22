d290abf28e8a5807826834eb394a483056542cbf
Pierre Cochard <pierre.cochard@inria.fr>
2024-11-22 11:04:12 +0100

    [vhdl] add faust2vhdl packages

--- config/targets/vhdl.mk
+++ config/targets/vhdl.mk
@@ -3,7 +3,7 @@ BD_TARGET := $(BD_FAUST2VHDL)
 
 get_nchannels_i := $(call set_preprocessor_data,$(INPUTS))
 get_nchannels_o := $(call set_preprocessor_data,$(OUTPUTS))
-
-PROJECT_DEPENDENCIES    += $(VHDL_TARGET)
-SCRIPT_HOST_CONFIG      :=  faust2vhdl
-RTL_SOURCES				+= $(VHDL_TARGET)
+PROJECT_DEPENDENCIES += $(VHDL_TARGET)
+SCRIPT_HOST_CONFIG   :=  faust2vhdl
+RTL_SOURCES	 += $(VHDL_TARGET)
+RTL_SOURCES	 += $(wildcard $(SOURCE_RTL_DIR)/faust2vhdl/*.vhd)
